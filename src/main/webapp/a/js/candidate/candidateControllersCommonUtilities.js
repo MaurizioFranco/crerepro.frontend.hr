ed6c527b014a26c08ea2c1af863c38f2537fab6f
MaurizioFranco <maurizio.franco@ymail.com>
2020-05-07 16:04:43 +0200

    Updated note api uri

--- src/main/webapp/a/js/candidate/candidateControllersCommonUtilities.js
+++ src/main/webapp/a/js/candidate/candidateControllersCommonUtilities.js
@@ -1,6 +1,5 @@
 var candidateResourceApi2 = MAIN_BACKEND_LOCATION+'/api/v1/candidatecustom/';
 var courseResourceApi2 = MAIN_BACKEND_LOCATION+'/api/v1/coursepage/codes';
-var NoteTemplateResourceApi = '/centauri/api/v1/note/';
+var NoteTemplateResourceApi = MAIN_BACKEND_LOCATION+'/api/v1/note/';
 var originSiteResourceApi = MAIN_BACKEND_LOCATION+'/api/v1/originsitescustom/';
-//add by Seba
 var candidateStatesApi = MAIN_BACKEND_LOCATION+'/api/v1/candidateStates/';
